3cba47ce9b3ae6e4aca0856b7cb00022aae1aac7
karthikjp08-cmyk <karthikjp08@gmail.com>
2026-05-07 13:07:21 +0530

    Update server.js

--- server.js
+++ server.js
@@ -24,13 +24,8 @@ const ai = new GoogleGenAI({
 });
 
 const MODELS = [
-    "gemini-2.5-flash",
-    "gemini-2.5-flash-lite",
-    "gemini-2.5-pro",
     "gemini-2.0-flash",
-    "gemini-2.0-flash-lite",
-    "gemini-1.5-pro-latest",
-    "gemini-1.5-flash-latest"
+    "gemini-1.5-flash"
 ];
 
 let memory = [];
